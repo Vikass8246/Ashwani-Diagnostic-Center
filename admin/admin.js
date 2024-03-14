f0edfb5f80e4b79dad9bd2b3dabb63d7de48119a
Vikas <79444074+Vikass8246@users.noreply.github.com>
2024-03-15 01:26:25 +0530

    Delete admin directory

--- admin/admin.js
+++
@@ -1,8 +0,0 @@
-function myfunction() {
-    var x = document.getElementById("Patient-view");
-    if (x.style.display === "none") {
-      x.style.display = "block";
-    } else {
-      x.style.display = "none";
-    }
-  }
